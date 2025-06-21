c608b45a3296c0aa1a785bfe4dfb5a6fdfabb7f9
yasintaades <148522651+yasintaades@users.noreply.github.com>
2025-06-22 00:02:15 +0700

    Add files via upload

---
+++ src/vendor/aymanalhattami/filament-slim-scrollbar/src/FilamentSlimScrollbarServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Aymanalhattami\FilamentSlimScrollbar;
+
+use Filament\Support\Assets\Css;
+use Filament\Support\Facades\FilamentAsset;
+use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
+
+class FilamentSlimScrollbarServiceProvider extends PackageServiceProvider
+{
+    public static string $name = 'filament-slim-scrollbar';
+
+    public function configurePackage(Package $package): void
+    {
+        $package->name(static::$name)
+            ->hasAssets();
+
+        FilamentAsset::register(
+            assets: [
+                Css::make(static::$name, __DIR__.'/../resources/dist/app.css'),
+            ],
+            package: 'aymanalhattami/filament-slim-scrollbar'
+        );
+    }
+}
